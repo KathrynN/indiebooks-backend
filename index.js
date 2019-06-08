349e23bc8621503d2e7a3c161ff7dcab6a824d60
Kathryn Newbould <kathrynrnewbould@gmail.com>
2019-06-08 09:04:57 +0200

    Working backend api to wrap goodreads info requests

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
 var express = require("express");
+const axios = require('axios');
+var parser = require('xml2json');
+
+require('dotenv').config();
+
+
 var app = express();
 app.listen(3001, () => {
- console.log("Server running on port 3000");
+ console.log("Server running on port 3001");
+});
+app.get("/getbookinfo", (req, res, next) => {
+  axios.get("https://www.goodreads.com/book/show?key=" + process.env.REACT_APP_GOODREADS
+  + "&id=" + req.query.grid).then(
+    result => {
+      var json = parser.toJson(result.data);
+      res.json(json);
+    },
+    error => {
+      console.log(error);
+      res.json(["Something appears to have gone wrong"]);
+    }
+  )
 });
