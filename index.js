46f598440fccecbe065664d08628e1a4d6120291
Kathryn Newbould <kathrynrnewbould@gmail.com>
2019-06-09 10:23:07 +0200

    Added post endpoint for adding additional books

--- index.js
+++ index.js
@@ -5,12 +5,14 @@ var cors = require('cors');
 var fs = require('fs');
 var async = require('async');
 var _ = require('underscore');
-
+var bodyParser = require('body-parser');
 require('dotenv').config();
 
 
 var app = express();
 app.use(cors());
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
 app.listen(3001, () => {
  console.log("Server running on port 3001");
@@ -23,6 +25,17 @@ app.get("/getbookinfo", (req, res, next) => {
 app.get("/getallbooks", (req, res, next) => {
   var allBooks = JSON.parse(fs.readFileSync('allBooks.json', 'utf8')).books;
   allBooks = _.shuffle(allBooks);
+  getInfoForAllBooksFromGoodreads(allBooks, res);
+})
+
+app.post('/addbook', function(req, res) {
+    var book_id = req.body.bookId;
+    console.log("received request to include " + book_id)
+    // TODO include book id xD
+    res.send(book_id);
+});
+
+function getInfoForAllBooksFromGoodreads(allBooks, res) {
   const responses = [];
 
   var promises = allBooks.map((grid) => {
@@ -36,17 +49,18 @@ app.get("/getallbooks", (req, res, next) => {
   });
 
   Promise.all(promises)
-    .then(result => res.json({}.books = responses))
+    .then(result => {
+      res.json({}.books = responses);
+      console.log("serviced all book request");
+    })
     .catch(console.error)
-
-})
+}
 
 function getInfoFromGoodreads(grid, lamda) {
   axios.get("https://www.goodreads.com/book/show?key=" + process.env.REACT_APP_GOODREADS
   + "&id=" + grid).then(
     result => {
       var json = parser.toJson(result.data);
-      console.log("WORKING:", json)
       lamda(json);
     },
     error => {
